2b4f130f72720d48a99d0c537aabea8e4149d446
Al Oletres <alejandrooletres@gmail.com>
2022-10-19 07:02:15 +0800

    feature: added a logger for headers value

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -3,6 +3,7 @@ import {Request, Response, NextFunction} from "express"
 export const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log('\n')
   console.log(`Start Request [${req.method} - ${req.url}]`)
+  console.log(`Headers: `, req.headers)
   console.log("Payload: ", req.body)
   console.log("Params: ", req.params)
   console.log(`End Request [${req.method} - ${req.url}]`)
