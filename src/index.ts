64b88f6dfac388d8be7ca3b98d9fd5befdae03e3
LodiAniez <lodianiez@gmail.com>
2022-12-14 16:25:09 +0800

    update: temp removed db connection for test; added test root endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express, { Express } from "express"
+import express, { Express, Request, Response } from "express"
 import cors from "cors"
 import path from "path"
 import serverless from "serverless-http"
 import {PORT, NODE_ENV} from "./configs"
-import {router, databaseConnect, logger, upload} from "./middlewares"
+import {router, logger, upload} from "./middlewares"
+// databaseConnect
 
 const app: Express = express()
 
@@ -13,9 +14,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(upload.array("images"))
 app.use(logger)
-app.use(databaseConnect)
+// app.use(databaseConnect)
 
 router(app)
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send({
+    message: "Gwapo ko"
+  })
+})
 
 if (NODE_ENV && NODE_ENV === "development") {
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
