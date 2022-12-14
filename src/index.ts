6dada3c79112b858646f7bc8b2755a5333eeffe3
LodiAniez <lodianiez@gmail.com>
2022-12-14 17:22:40 +0800

    update: updated test endpoint

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(logger)
 
 router(app)
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/staging", (req: Request, res: Response) => {
   res.status(200).send({
     message: "Gwapo ko"
   })
